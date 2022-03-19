6d3935a854c764c4ad8557e83cb6dca9a22f25e7
Arshiya Kiani <zytell3301@gmail.com>
2022-03-20 01:37:41 +0330

    Update Dependencies type and NewMessagesCore method

--- internal/core/core.go
+++ internal/core/core.go
@@ -1,18 +1,23 @@
 package core
 
+import uuid_generator "github.com/zytell3301/uuid-generator"
+
 type Service struct {
-	repository Repository
+	repository    Repository
+	uuidGenerator uuid_generator.Generator
 }
 
 type ServiceConfig struct {
 }
 
 type Dependencies struct {
-	Repository Repository
+	Repository    Repository
+	UuidGenerator uuid_generator.Generator
 }
 
 func NewMessagesCore(config ServiceConfig, dependencies Dependencies) Service {
 	return Service{
-		repository: dependencies.Repository,
+		repository:    dependencies.Repository,
+		uuidGenerator: dependencies.UuidGenerator,
 	}
 }

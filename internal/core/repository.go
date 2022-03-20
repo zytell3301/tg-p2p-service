9abb9a5de314a5ad79bdacb2e4cf05ad0400005a
Arshiya Kiani <zytell3301@gmail.com>
2022-03-21 00:54:46 +0330

    Add RecordMessage method to repository interface

--- internal/core/repository.go
+++ internal/core/repository.go
@@ -8,4 +8,5 @@ import (
 type Repository interface {
 	NewContact(contact domain.Contact) error // @TODO The contact must be flipped and reinserted into database
 	GetContacts(uuid uuid.UUID) ([]domain.Contact, error)
+	RecordMessage(message domain.Message) error //@TODO flip message sides and reinsert into database
 }

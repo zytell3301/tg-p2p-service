8cc5262d5d799fc7855ed63f155b1a6f36c8a00f
Arshiya Kiani <zytell3301@gmail.com>
2022-03-21 00:47:21 +0330

    Add todo to repository interface

--- internal/core/repository.go
+++ internal/core/repository.go
@@ -6,6 +6,6 @@ import (
 )
 
 type Repository interface {
-	NewContact(contact domain.Contact) error
+	NewContact(contact domain.Contact) error // @TODO The contact must be flipped and reinserted into database
 	GetContacts(uuid uuid.UUID) ([]domain.Contact, error)
 }

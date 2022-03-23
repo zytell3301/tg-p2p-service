e5d2275a635dd4ab1a2a59e342e0402471ef8a08
Arshiya Kiani <zytell3301@gmail.com>
2022-03-24 00:59:03 +0430

    Add GetMessage method to repository interface

--- internal/core/repository.go
+++ internal/core/repository.go
@@ -3,10 +3,12 @@ package core
 import (
 	"github.com/google/uuid"
 	"tg-p2p-service/internal/domain"
+	"time"
 )
 
 type Repository interface {
 	NewContact(contact domain.Contact) error // @TODO The contact must be flipped and reinserted into database
 	GetContacts(uuid uuid.UUID) ([]domain.Contact, error)
 	RecordMessage(message domain.Message) error //@TODO flip message sides and reinsert into database
+	GetMessages(from time.Time, to time.Time, leftSide uuid.UUID, contactId uuid.UUID) ([]domain.Message, error)
 }

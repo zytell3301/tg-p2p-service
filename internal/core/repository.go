182f72d4b04a8df0b574fb81325884effdafab58
Arshiya Kiani <zytell3301@gmail.com>
2022-03-28 02:06:21 +0430

    Rename a parameter to a proper name

--- internal/core/repository.go
+++ internal/core/repository.go
@@ -8,7 +8,7 @@ import (
 
 type Repository interface {
 	NewContact(contact domain.Contact) error // @TODO The contact must be flipped and reinserted into database
-	GetContacts(uuid uuid.UUID) ([]domain.Contact, error)
+	GetContacts(userId uuid.UUID) ([]domain.Contact, error)
 	RecordMessage(message domain.Message) error //@TODO flip message sides and reinsert into database
 	GetMessages(from time.Time, to time.Time, leftSide uuid.UUID, contactId uuid.UUID) ([]domain.Message, error)
 	UpdateMessage(message domain.Message) error // @TODO make sure that both message replications will get updated

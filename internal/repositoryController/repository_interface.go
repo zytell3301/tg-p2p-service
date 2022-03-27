9be2e5cb9fb49b016b3e191c03f9f137ba496c5d
Arshiya Kiani <zytell3301@gmail.com>
2022-03-28 02:22:18 +0430

    Add RecordMessage method to repository_interface interface

--- internal/repositoryController/repository_interface.go
+++ internal/repositoryController/repository_interface.go
@@ -8,6 +8,7 @@ import (
 type Repository interface {
 	AddContact(contact domain.Contact) (AddContactBatch, error)
 	GetContacts(userId uuid.UUID) ([]domain.Contact, error)
+	RecordMessage(message domain.Message) (RecordMessageBatch, error)
 }
 
 type AddContactBatch interface {
@@ -16,6 +17,18 @@ type AddContactBatch interface {
 	// If batch feature is not available, you can separately run the queries
 	// but you will lose the database consistency on failure on any operation.
 	AddContactToBatch(contact domain.Contact) error
+
+	// After adding queries to batch, you must call ExecuteOperation method
+	// or the changes will be lost
+	ExecuteOperation() error
+}
+
+type RecordMessageBatch interface {
+	// AddMessageToBatch only adds a query to batch and won't execute the batch.
+	// You must explicitly call ExecuteOperation method to execute the batch.
+	// If batch feature is not available, you can separately run the queries
+	// but you will lose the database consistency on failure on any operation.
+	AddMessageToBatch(message domain.Message) error
 
 	// After adding queries to batch, you must call ExecuteOperation method
 	// or the changes will be lost

9b69fd407b313985f5122e744928c428bb78cf6e
Arshiya Kiani <zytell3301@gmail.com>
2022-03-28 03:20:53 +0430

    Update message logger interface

--- internal/core/message_logger.go
+++ internal/core/message_logger.go
@@ -4,5 +4,7 @@ import "tg-p2p-service/internal/domain"
 
 // Message logger logs any message changes such as deleting or editing a message.
 type MessageLogger interface {
-	LogMessage(message domain.Message)
+	LogOneWayDeletedMessage(message domain.Message) error
+	LogTwoWayDeletedMessage(message domain.Message) error
+	LogEditedMessage(message domain.Message) error
 }

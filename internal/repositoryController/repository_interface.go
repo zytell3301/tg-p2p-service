96d6616a0df73230b33b610578cdf00c4e09bab4
Arshiya Kiani <zytell3301@gmail.com>
2022-03-28 03:02:07 +0430

    Add BatchBase type and replace redundant codes

--- internal/repositoryController/repository_interface.go
+++ internal/repositoryController/repository_interface.go
@@ -16,46 +16,44 @@ type Repository interface {
 	DeleteMessage(message domain.Message) (DeleteMessageBatch, error)
 }
 
+type BatchBase interface {
+	// After adding queries to batch, you must call ExecuteOperation method
+	// or the changes will be lost
+	ExecuteOperation() error
+}
+
 type AddContactBatch interface {
+	BatchBase
+
 	// AddContactToBatch only adds a query to batch and won't execute the batch.
 	// You must explicitly call ExecuteOperation method to execute the batch.
 	// If batch feature is not available, you can separately run the queries
 	// but you will lose the database consistency on failure on any operation.
 	AddContactToBatch(contact domain.Contact) error
-
-	// After adding queries to batch, you must call ExecuteOperation method
-	// or the changes will be lost
-	ExecuteOperation() error
 }
 
 type RecordMessageBatch interface {
+	BatchBase
+
 	// AddMessageToBatch only adds a query to batch and won't execute the batch.
 	// You must explicitly call ExecuteOperation method to execute the batch.
 	// If batch feature is not available, you can separately run the queries
 	// but you will lose the database consistency on failure on any operation.
 	AddMessageToBatch(message domain.Message) error
-
-	// After adding queries to batch, you must call ExecuteOperation method
-	// or the changes will be lost
-	ExecuteOperation() error
 }
 
 type UpdateMessageBatch interface {
+	BatchBase
+
 	// This method will be used to add the update query for flipped message to batch.
 	// This method just adds a query to batch and won't execute it
 	AddUpdateToBatch(message domain.Message) error
-
-	// After adding queries to batch, you must call ExecuteOperation method
-	// or the changes will be lost
-	ExecuteOperation() error
 }
 
 type DeleteMessageBatch interface {
+	BatchBase
+
 	// Adds delete operation to batch.
 	// This method just adds a query to batch and won't execute it.
 	AddDeleteToBatch(message domain.Message) error
-
-	// After adding queries to batch, you must call ExecuteOperation method
-	// or the changes will be lost
-	ExecuteOperation() error
 }
